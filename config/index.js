ad0a1e2905f0fb083e055b588e11a8271da0e876
Loc Thanh <locnt19@gmail.com>
2020-03-17 00:05:53 +0700

    01 db connection

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const db_user = 'admin';
+const db_password = 'admin';
+
+const MONGODB_URI = `mongodb+srv://${db_user}:${db_password}@cluster0-mfqk2.gcp.mongodb.net/mern-stack`;
+
+module.exports = MONGODB_URI;
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 5000;
+
+
+// require db connection
+require('./models');
 
 // configure body parser for AJAX requests
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+
+
+app.use(routes);
+
 
 // routes
 app.get('/', (req, res) => {
